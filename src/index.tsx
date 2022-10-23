8792627dce687b03fb0c91cc71c27d02448cec3e
tnty <tnty.working@gmail.com>
2022-10-23 15:28:47 +0300

    feat: global css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./main.scss"
+import "./index.scss"
 
 const root = createRoot(document.getElementById('root')!);
 
