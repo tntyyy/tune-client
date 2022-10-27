71d3eb38565de84a429a685b2505a08ac7a8b614
tnty <tnty.working@gmail.com>
2022-10-27 17:22:58 +0300

    feat: signup page

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,10 +1,40 @@
 import React, { FC } from "react";
 import styles from "./SignUpPage.module.scss";
+import { navigationLinks } from "@/utils/navigation";
+import Navigation from "@/containers/Navigation/Navigation";
+import SignUpForm from "@/containers/SignUpForm/SignUpForm";
+import signUpIllustration from "@/assets/images/signUpIllustration.svg";
 
-interface SignUpPageProps {}
+const SignUpPage: FC = () => {
+    const onSubmitForm = (e: React.MouseEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("submit");
+    };
 
-const SignUpPage: FC<SignUpPageProps> = (props) => {
-    return <h1>sign</h1>;
+    return (
+        <div className={styles.main}>
+            <Navigation links={navigationLinks} />
+            <div className={styles.content}>
+                <div className={styles.info}>
+                    <h2 className={styles.info__title}>
+                        <span>Tune</span> is a digital music service
+                    </h2>
+                    <img
+                        src={signUpIllustration}
+                        alt="Sign Up"
+                        className={styles.info__image}
+                    />
+                </div>
+                <div className={styles.form}>
+                    <h2 className={styles.form__title}>Let`s join</h2>
+                    <p className={styles.form__subtitle}>
+                        Make an account for the best experience
+                    </p>
+                    <SignUpForm handleSubmit={onSubmitForm} />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default SignUpPage;
