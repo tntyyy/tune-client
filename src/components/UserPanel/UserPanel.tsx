265f7616578edab04c30c6a426d60e604261c39d
tnty <tnty.working@gmail.com>
2022-10-27 18:20:44 +0300

    fix: user panel change icon

--- src/components/UserPanel/UserPanel.tsx
+++ src/components/UserPanel/UserPanel.tsx
@@ -1,14 +1,14 @@
 import React, { FC } from "react";
 import styles from "./UserPanel.module.scss";
 import { Link } from "react-router-dom";
-import userPic from "@/assets/images/defaultUserPic.png";
+import defaultUserPic from "@/assets/images/defaultUserPic.svg";
 import { AppRoutesEnum } from "@/routes/types";
 
 const UserPanel: FC = () => {
     return (
         <div className={styles.user}>
             <Link to={AppRoutesEnum.SIGNUP}>
-                <img src={userPic} alt="User" />
+                <img src={defaultUserPic} alt="User" />
             </Link>
         </div>
     );
