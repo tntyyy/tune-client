85c3b19f9ce9d3ca01e7466eabad4df94505bdf1
tnty <tnty.working@gmail.com>
2022-11-06 18:47:37 +0300

    feat: success  notifications after sign up/in

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import AppRoutes from "@/routes";
 import React, { FC } from "react";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 const App: FC = () => {
     return (
         <BrowserRouter>
             <AppRoutes />
+            <ToastContainer />
         </BrowserRouter>
     );
 };
