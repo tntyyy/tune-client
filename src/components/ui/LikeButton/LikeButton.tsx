81c235062ff395eb84a6e568e089e94e70e2ee88
tnty <tnty.working@gmail.com>
2022-11-18 19:03:17 +0300

    feat: public & private routes

--- src/components/ui/LikeButton/LikeButton.tsx
+++ src/components/ui/LikeButton/LikeButton.tsx
@@ -1,7 +1,5 @@
 import React, { FC, useState } from "react";
 
-import styles from "./LikeButton.module.scss";
-
 interface LikeButtonProps {
     onLikeClick: () => void;
 }
--- src/components/ui/PlayButton/PlayButton.tsx
+++ src/components/ui/PlayButton/PlayButton.tsx
@@ -3,8 +3,6 @@ import React, { FC, useState } from "react";
 import pauseIcon from "@/assets/icons/pause.svg";
 import playIcon from "@/assets/icons/play.svg";
 
-import styles from "./PlayButton.module.scss";
-
 interface PlayButtonProps {
     onPlayClick: () => void;
 }
